1a8c32e64982454889ee8d19eeba88c594be1007
suran5aev <ilimkulovmaksat@gmail.com>
2024-05-03 16:38:51 +0600

    update

--- lesson_2month/hw2_month/hw5.py
+++ lesson_2month/hw2_month/hw5.py
@@ -32,3 +32,35 @@ print (num1 - num2)
 print (num1 * num2)
 print (num1 / num2)
 print (num1 // num2)
+
+
+
+
+#Доп дз 
+class Book:
+    def __init__(self,title,author,year):
+        self.title = title
+        self.author = author
+        self.year = year
+    def __lt__ (self,other):
+        return self.year < other.year
+    def __eg__(self,other):
+        return self.year == other.year
+    def __le__(self,other):
+        return self.year <= other.year
+    def __gt__(self,other):
+        return self.year > other.year
+    def __ne__(self,other):
+        return self.year != other.year 
+    def __str__(self):
+        return f"{self.title},{self.author},{self.year}"
+book1 = Book ("ЧУДО", 'Я' ,2024)
+book2 = Book ('бизнес','Элиза',2022)
+print(book1)
+print(book2)
+print(book1 < book2)
+print(book1 == book2)
+print(book1 <= book2)
+print(book1 > book2)
+print(book1 != book2)
+
